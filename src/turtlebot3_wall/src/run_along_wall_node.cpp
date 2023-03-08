776b090ad16cbc96a119a13d7271ef346e68c659
makotoyoshigoe <91446273+makotoyoshigoe@users.noreply.github.com>
2023-03-08 14:43:21 +0900

    Feature/run along wall 23 3 8 14 25 (#16)
    * Rename from sub_laser to run_along_wall
    
    * Add param file
    
    * Change lidar-range-data update rate
    
    * sub_laserからrun_along_wallに名前を変更、左側の壁に沿って走行する機能を追加

--- src/turtlebot3_wall/src/run_along_wall_node.cpp
+++ src/turtlebot3_wall/src/run_along_wall_node.cpp
@@ -1,8 +1,8 @@
-#include"turtlebot3_wall/sub_laser.hpp"
+#include"turtlebot3_wall/run_along_wall.hpp"
 
 int main(int argc, char* argv[]){
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<Turtlebot3Wall::SubLaser>();
+    auto node = std::make_shared<Turtlebot3Wall::RunAlongWall>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
