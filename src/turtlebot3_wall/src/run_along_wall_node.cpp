1521aa670db3a39137cbc197d34d227b684f5bed
makotoyoshigoe <91446273+makotoyoshigoe@users.noreply.github.com>
2023-03-08 18:04:23 +0900

    turtlebot3_wallをサブモジュールとしてsrc以下に追加 (#19)

--- src/turtlebot3_wall/src/run_along_wall_node.cpp
+++
@@ -1,9 +0,0 @@
-#include"turtlebot3_wall/run_along_wall.hpp"
-
-int main(int argc, char* argv[]){
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<Turtlebot3Wall::RunAlongWall>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
-}
